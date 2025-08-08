2b65aa203946929105a0f9afbfc1670e65e7cd8b
Antonio Alb <antonioalb99@gmail.com>
2025-08-08 12:37:38 +0300

    Initial commit with Terraform and GitHub Actions v9

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = "vm-win-notepadpp"
+  name                  = "vm-win-notepadp"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   size                  = "Standard_B2s"
