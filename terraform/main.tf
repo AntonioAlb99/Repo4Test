fe8d0f5ccd65bf65aeef52b23c66ff09c74250cd
Antonio Alb <antonioalb99@gmail.com>
2025-08-08 12:15:44 +0300

    Initial commit with Terraform and GitHub Actions v8

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "pip-win-notepadpp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   tags                = var.tags
 }
 
